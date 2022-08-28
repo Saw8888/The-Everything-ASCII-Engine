a1b478c56581c422736b3ae105e75a1463bca630
Jigsaw <71344333+Saw8888@users.noreply.github.com>
2022-08-28 18:07:19 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,13 +7,6 @@
 #define sleep(x) Sleep(1000 * (x))
 #endif
 
-/*
-w == 119
-a == 97
-s == 115
-d == 100
-*/
-
 int playerX = 10;
 int playerY = 0;
 
@@ -25,32 +18,12 @@ const int sand = 2;
 const int water = 3;
 
 char ch;
+int randNum;
 int itemChoice = 2;
 
-int grid[20][20] = {
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
-};
+int grid[20][20];
 
-void draw_screen(){
+void draw_screen(float percentage){
  system("cls");
  int y,x;
  for(y=0;y<sizeY;y++){
@@ -63,6 +36,9 @@ void draw_screen(){
    }
   }
   if(y == 1){
+  	printf("%f%% COMPLETED", percentage);
+  }
+  if(y == 2){
   	switch(itemChoice){
   		case 2:
   	  printf("Current Item : sand");
@@ -79,24 +55,29 @@ void draw_screen(){
  }
 }
 
+int randRange(int min, int max)
+{
+  return min + (int) (rand() / (double) (RAND_MAX + 1) * (max - min + 1));
+}
+
 void keyboardInput(){
 	if(kbhit()){
  	 ch = getch();
  	 switch(ch){
 	   case 'a':
-	   	playerX--;
+	   	if(playerX > 0 && grid[playerY][playerX-1]==dead){playerX--;}
 	   	break;
 	   	
 				case 'd':
-				 playerX++;
+					if(playerX < sizeX-1 && grid[playerY][playerX+1]==dead){playerX++;}
 				 break;
         
 				case 'w':
-				 playerY--;
+					if(playerY > 0 && grid[playerY-1][playerX]==dead){playerY--;}
 				 break;
        
 				case 's':
-				 playerY++;
+				 	if(playerY < sizeY-1 && grid[playerY+1][playerX]==dead){playerY++;}
 				 break;
 				   
 				case '1':
@@ -121,6 +102,7 @@ void keyboardInput(){
 }
 
 void simulate_sand(int grid[sizeX][sizeY],int x,int y){
+	randNum = randRange(0,1);
  if(grid[y][x] == dead || y >= sizeY - 1){ // do nothing if dead or already at the bottom of the screen
   return;
  }
@@ -128,39 +110,44 @@ void simulate_sand(int grid[sizeX][sizeY],int x,int y){
   grid[y][x] = dead;
   grid[y+1][x] = sand;
  }
- else if(x > 0 && grid[y+1][x-1] == dead){ // move down and left if empty
+ else if(x > 0 && grid[y+1][x-1] == dead && randNum == 0){ // move down and left if empty
   grid[y][x] = dead;
   grid[y+1][x-1] = sand;
  }
- else if(x < sizeX - 1 && grid[y+1][x+1] == dead){ // move down and right if empty
+ else if(x < sizeX - 1 && grid[y+1][x+1] == dead && randNum == 1){ // move down and right if empty
   grid[y][x] = dead;
   grid[y+1][x+1] = sand;
  }
 }
 
 void simulate_water(int grid[sizeX][sizeY],int x,int y){
+	randNum = randRange(0,1);
  if(grid[y][x] == dead || y >= sizeY - 1){ // do nothing if dead or already at the bottom of the screen
   return;
+ }
+ else if(grid[y+1][x] > 0 && grid[y+1][x+1] > 0 && grid[y+1][x-1] > 0 && grid[y][x+1] > 0 && grid[y][x-1] > 0 && randNum == 1){
+ 	grid[y][x] = dead;
+ 	grid[y-1][x] = water;
  }
  else if(grid[y+1][x] == dead){ // move down if empty
   grid[y][x] = dead;
   grid[y+1][x] = water;
  }
- else if(x > 0 && grid[y+1][x-1] == dead){ // move down and left if empty
+ else if(x > 0 && grid[y+1][x-1] == dead && randNum == 0){ // move down and left if empty
   grid[y][x] = dead;
   grid[y+1][x-1] = sand;
  }
- else if(x < sizeX - 1 && grid[y+1][x+1] == dead){ // move down and right if empty
+ else if(x < sizeX - 1 && grid[y+1][x+1] == dead && randNum == 1){ // move down and right if empty
   grid[y][x] = dead;
   grid[y+1][x+1] = sand;
  }
- else if(x > 0 && grid[y][x-1] == dead){
- 	grid[y][x] = dead;
- 	grid[y][x-1] = water;
- }
- else if(x < sizeX - 1 && grid[y][x+1] == dead){
+ else if(x < sizeX - 1 && grid[y][x+1] == dead && randNum == 0){
  	grid[y][x] = dead;
  	grid[y][x+1] = water;
+ }
+ else if(x > 0 && grid[y][x-1] == dead && randNum == 1){
+ 	grid[y][x] = dead;
+ 	grid[y][x-1] = water;
  }
 }
 
@@ -169,6 +156,7 @@ int main(void){
  
  for(iterations=0;iterations<1000;iterations++){
   keyboardInput();
+  float percentage = (float)iterations/1000*100;
   for(y=sizeY-1;y>=0;y--){
    for(x=sizeX-1;x>=0;x--){
 			 if(grid[y][x] == sand){
@@ -179,7 +167,7 @@ int main(void){
     }
    }
   }
- draw_screen();
- sleep(0.01);
+  draw_screen(percentage);
+  sleep(0.01);
  }
 }
